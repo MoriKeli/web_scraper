951f0fe9886bbc299517c0942a7768774c08f203
Mori Keli <kelimbth@gmail.com>
2022-12-01 10:10:41 +0300

    made a few changes

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 # scraping a html file
 # with open('index.html') as html:
@@ -35,28 +36,30 @@ import requests
 
 
 # looping through all articles
-# for article in soup.find_all('div', class_='col-12'):
-#     headline = article.h2
-#     article_content = article.p
-#     print(f'Article headline: {headline}')
-#     print(f'Content: {article_content}')
-#     print()
+
+
+for article in soup.find_all('div', class_='col-12'):
+    headline = article.h2
+    article_content = article.p
+    print(f'Article headline: {headline}')
+    print(f'Content: {article_content}')
+    print()
 
 
 # getting info from a live website
 
-""" Here I used a blog website I developed with Django """
+# """ Here I used a blog website I developed with Django """
 
-src = requests.get(url='http://127.0.0.1:8000/users/homepage/').text     # .text is used to get source code from the suggested page
-soup = BeautifulSoup(src, 'lxml')
+# src = requests.get(url='http://127.0.0.1:8000/users/homepage/').text     # .text is used to get source code from the suggested page
+# soup = BeautifulSoup(src, 'lxml')
 
-# print(soup.prettify())
+# # print(soup.prettify())
 
-# Grabbing all article
-for article in soup.find_all('article', class_='content-section'):
-    headline = article.h4
-    body_text = article.p
-    print(f'Article: {headline}')
-    print(f'Content: {body_text}')
-    print()
+# # Grabbing all article
+# for article in soup.find_all('article', class_='content-section'):
+#     headline = article.h4
+#     body_text = article.p
+#     print(f'Article: {headline}')
+#     print(f'Content: {body_text}')
+#     print()
 
